b183ce5e23e069b53b061b041f0630b2063c2dfb
ektha4903 <82365670+ektha4903@users.noreply.github.com>
2021-04-12 17:35:37 +0900

    Update Hello.java

--- src/basic/Hello.java
+++ src/basic/Hello.java
@@ -6,12 +6,10 @@ public class Hello {
 		System.out.println("Hello");  
 		System.out.println("안녕하세요");
 		System.out.println("반갑습니다");
-
 		System.out.println("비가 오네요");
-
 		System.out.println("오늘은");
 		System.out.println("월요일입니다");
-
 		System.out.println("githup연결");
+		System.out.println("githup연결2");
       }
 }
