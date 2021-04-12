c6fd9936b68e0dce8bcf091392e050bf1afcbf7c
dasomjoo <ektha4903@gmail.com>
2021-04-12 15:17:28 +0900

    hello update

--- src/basic/Hello.java
+++ src/basic/Hello.java
@@ -3,6 +3,8 @@ package basic;
 public class Hello {
 	public static void main(String[] args) {
 //		주석(코드설명, 개발자가 원하는 정보)을 처리
-		System.out.println("Hello");    
+		System.out.println("Hello");  
+		System.out.println("안녕하세요");
+		System.out.println("반갑습니다");
       }
 }
